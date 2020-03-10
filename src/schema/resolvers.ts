b91b1f8d79fc052bf3ba45b80b0588a53202eff4
Yunsuk Hong <yunze89@apparelbase.com>
2020-03-10 13:24:21 +0900

    주석 수정

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -2,34 +2,34 @@ import { IResolvers } from 'graphql-tools';
 // import Posts from '../hardcode/post';   // 하드코딩 db 데이터 (db에서 fetch한 데이터 모델이라고 가정)
 // import Users from '../hardcode/user';   // 하드코딩 db 데이터 (db에서 fetch한 데이터 모델이라고 가정)
 
-// graphql resolver 구현
-// 실제 DB에서 데이터를 fetch하는 예제는 아니므로, resolver 정의하는 로직만 대략 파악하길 바람.
-// resolver 함수의 paramter는 총 4개 - (root, args, context, info) 따라서 query의 argument를 전달받기 위해선 두번째 파라미터를 사용
+// graphql resolver 구현부
+// resolver 함수의 paramter는 총 4개 - (root, args, context, info)
+// query의 argument를 전달받기 위해선 두번째 파라미터를 사용
+// prisma client를 사용하기 위해선 세번째 파라미터를 사용
 
 const resolvers :IResolvers = {
     Post:{
-        // post type의 User(post의 writer field) 객체 접근하기 위한 resolver
+        // post type의 User(post의 writer field) 객체 접근하기 위한 resolver : parent는 Post 객체
         // prisma client 인스턴스의 메서드 이용
-        writer: (parent, args, context) => context.prisma.post({id:parent.id}).writer()
+        writer: (parent, _, context) => context.prisma.post({id:parent.id}).writer()
 
         // id, title 등 다른 field들은 db에서 fetch 했다고 가정한 데이터 모델과 field가 1:1 매핑 되므로 정의해주지 않아도 매핑됨
     },
     User:{
-        /* user type의 [Post](user의 posts field) 객체 배열에 사용된 Post 객체 접근하기 위한 resolver 함수
-         : user 객체의 id 값으로 posts 목록 조건 검색*/
-
+        // user type의 [Post](user의 posts field) 객체 배열에 사용된 Post 객체 접근하기 위한 resolver 함수 : parent는 User 객체
         // prisma client 인스턴스의 메서드 이용
-        posts: (parent,user,context) => context.prisma.user({id:parent.id}).posts()
+        posts: (parent, _,context) => context.prisma.user({id:parent.id}).posts()
 
         // id, name 등 다른 field들은 db에서 fetch 했다고 가정한 데이터 모델과 field가 1:1 매핑 되므로 정의해주지 않아도 매핑됨
     },
     Query:{
         // prisma client 인스턴스의 메서드 이용
         posts: (_, args, context) => context.prisma.posts(),
-        post: (_,{id}, context) => context.prisma.post( {id}),
-        user: (_,{id}, context) => context.prisma.user({id}),
+        post: (_, {id}, context) => context.prisma.post( {id}),
+        user: (_, {id}, context) => context.prisma.user({id}),
     },
     Mutation: {
+        // 게시물 작성
         writePost: (_, {writer, title, description, tags}, context) =>
             // prisma client 인스턴스의 메서드 이용
             context.prisma.createPost({
@@ -38,6 +38,7 @@ const resolvers :IResolvers = {
                 description,
                 tags
             }),
+        // 회원가입
         signUp: (_, {email, pw, name}, context) =>
             // prisma client 인스턴스의 메서드 이용
             context.prisma.createUser({
@@ -46,6 +47,6 @@ const resolvers :IResolvers = {
                 name
             })
     }
-}
+};
 
 export default resolvers;
