58b8c60dd78ec4337c1ecde7e26524e3a4547008
Yunsuk Hong <yunze89@gmail.com>
2020-04-04 23:20:04 +0900

    Revert "Merge branch 'master' into conversion"
    This reverts commit 80f733f0952b9e5a06a80aad5553b1c1a56e9ca3, reversing
    changes made to 00c71c9ff129b099593545c5a34a9954b9cae4f8.

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -1,5 +1,10 @@
+/*export class Post {
+    writer(parent:any, _:any, context:any){
+        return context.prisma.post({id:parent.id}).writer();
+    }
+}*/
 export const Post = {
     writer : (parent:any, _:any, context:any)=>{
         return context.prisma.post({id:parent.id}).writer();
     }
-};
+}
--- src/shcema.ts
+++
@@ -1,8 +0,0 @@
-import typeDefs from './schema/type';
-import resolvers from './resolvers';
-import {makeExecutableSchema} from 'graphql-tools';
-
-export const executableSchema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-});
