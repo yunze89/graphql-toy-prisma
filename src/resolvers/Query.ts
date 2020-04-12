b037a2ddb2eb10048ba818a116eafe91ebba1789
Yunsuk Hong <yunze89@gmail.com>
2020-04-12 22:39:46 +0900

    페이징, 정렬, 개수 조회 추가 - 테스트 완료
    -schema type 수정
    -query resolver 수정

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -5,23 +5,42 @@ import {APP_SECRET} from '../const/const';
 export const Query = {
     // prisma client 인스턴스의 메서드 이용
     posts: async (_:any, args:any, context:any) => {
+        // filter 조건 추가
         const where = args.filter ? {
                 OR: [
                     { description_contains: args.filter},   // description 포함
                     { title_contains: args.filter}          // title 포함되어 있는 것 filtering
                 ],
             } : {};
+
+        // paging 위한 argument - Offset(시작 인덱스)/Limit(개수)을 이용한 방법
+        // skip - Offset : Prisma API상 Offset(Start) 시작 인덱스를 뜻하며, 해당 개수만큼 건너뛴다는 의미이다. (Default : 0)
+        // first - Limit : Limit은 Prisma API상 first에 해당된다. 제공된 시작 인덱스 이후 등장하는 X개 요소 가져온다.
+        const {skip, first, orderBy} = args;
+
+        // post 목록
         const posts = await context.prisma.posts({
-            where
+            where,
+            skip,
+            first,
+            orderBy
         });
-        return posts;
+
+        // 현재 데이터베이스 상에 저장된 Post 요소 총 개수
+        const count = await context.prisma
+            .postsConnection({where})
+            .aggregate()
+            .count();
+
+        // 최종 Posts 객체 반환
+        return {posts,count};
     },
     post: (_:any, {id}:any, context:any) => context.prisma.post( {id}),
     user: (_:any, {id}:any, context:any) => context.prisma.user({id}),
 
     // 로그인
     signIn: async(_:any, args:any, context:any) => {
-        // email로 user query (type정의시 email을 @unique로 지정 필요)
+        // email로 user query (type 정의 시 email을 @unique로 지정 필요)
         const user = await context.prisma.user({
             email: args.email
         });
--- src/schema/type.ts
+++ src/schema/type.ts
@@ -17,11 +17,15 @@ const typeDefs = `
         writer: User!               #해당 field를 위한 resolver 함수 구현
         tags: [String]
     }
+    type Posts{
+        posts: [Post]               #posts list 조회
+        count: Int!                #post 목록의 개수
+    }
     ## Query 정의 (조회) : entry point
     type Query{
-        posts(filter: String): [Post]               #posts list 조회
-        post(id: ID): Post          #argument query(post id 조건 검색)
-        user(id: ID): User          #argument query(user id 조건 검색)
+        posts(filter: String, skip: Int, first: Int, orderBy: PostOrderByInput): Posts               #posts list 조회
+        post(id: ID): Post                                                 #argument query(post id 조건 검색)
+        user(id: ID): User                                                 #argument query(user id 조건 검색)
         signIn(email: String!, pw: String!): AuthPayload
     }
     ## Mutation 정의 (수정성 요청) : entry point
@@ -32,6 +36,13 @@ const typeDefs = `
     ## Subscription 정의 (실시간 push)
     type Subscription{
         newPost: Post
+    }
+    #정렬위한 열거자 정의 - prisma api에서의 orderBy 참조
+    enum PostOrderByInput {
+        description_ASC
+        description_DESC
+        title_ASC
+        title_DESC
     }
 `;
 
